e5644dc4adaf3b5edccd3160b115fe9ad8c8946d
alenscout <alenscout@gmail.com>
2024-09-29 02:37:43 +0600

    added inline keyboard

--- app/handlers.py
+++ app/handlers.py
@@ -1,12 +1,13 @@
 from aiogram import F, Router
 from aiogram.types import Message
 from aiogram.filters import Command, CommandStart
+import app.keyboards as kb
 
 router = Router()
 
 @router.message(CommandStart())
 async def start(message: Message):
-    await message.answer("Используй команду /help для списка команд, либо выбери команду в меню")
+    await message.answer("Используй команду /help для списка команд, либо выбери команду в меню", reply_markup=kb.settings)
 
 @router.message(Command('help'))
 async def help(message: Message):
--- app/keyboards.py
+++ app/keyboards.py
@@ -1,6 +1,8 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
 main = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text='Каталог')],
     [KeyboardButton(text='Корзина'), KeyboardButton(text='Контакты')]
 ])
+
+settings = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='YouTube' , url='https://youtube.com')]])
