7e129aa5d049798325698c1cf11f4f19d62bfcd8
alenscout <alenscout@gmail.com>
2024-09-29 15:51:34 +0600

    added callback

--- app/handlers.py
+++ app/handlers.py
@@ -1,5 +1,5 @@
 from aiogram import F, Router
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from aiogram.filters import Command, CommandStart
 import app.keyboards as kb
 
@@ -7,8 +7,12 @@ router = Router()
 
 @router.message(CommandStart())
 async def start(message: Message):
-    await message.answer("Используй команду /help для списка команд, либо выбери команду в меню", reply_markup=kb.settings)
+    await message.answer("Используй команду /help для списка команд, либо выбери команду в меню", reply_markup=kb.main)
 
-@router.message(Command('help'))
-async def help(message: Message):
-    await message.answer('Список команд: /help, /start, /list')
+# @router.message(Command('help'))
+# async def help(message: Message):
+#     await message.answer('Список команд: /help, /start, /list')
+
+@router.callback_query(F.data == 'commands')
+async def commands(callback: CallbackQuery):
+    await callback.message.answer('Список команд: /help, /start, /list')
--- app/keyboards.py
+++ app/keyboards.py
@@ -1,8 +1,9 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
-main = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text='Каталог')],
-    [KeyboardButton(text='Корзина'), KeyboardButton(text='Контакты')]
-])
+# main = ReplyKeyboardMarkup(keyboard=[
+#     [KeyboardButton(text='Каталог')],
+#     [KeyboardButton(text='Корзина'), KeyboardButton(text='Контакты')]
+# ])
 
-settings = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='YouTube' , url='https://youtube.com')]])
+main = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='Сервис', callback_data='service')],
+                                             [InlineKeyboardButton(text='Команды', callback_data='commands')]])
