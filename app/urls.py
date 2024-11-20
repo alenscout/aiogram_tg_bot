41e7ccac494adbe2c7391999fa14e48ec5c55d6b
alenscout <alenscout@gmail.com>
2024-11-21 01:05:40 +0600

    url testik

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,29 @@
 import os, requests
 # Путь к файлу .env
 env_path = "/home/ubuntu/aiogram_tg_bot/.env"
-response = requests.get("https://ifconfig.me")
-public_ip = response.text.strip()
 
-with open(env_path, "a") as f:
-    f.write(f"SERVER_IP={public_ip}\n")
+# Попробуем получить IP из .env
+if os.path.exists(env_path):
+    with open(env_path, "r") as f:
+        lines = f.readlines()
+        # Ищем строку с SERVER_IP
+        for line in lines:
+            if line.startswith("SERVER_IP="):
+                public_ip = line.split('=')[1].strip()
+                break
+        else:
+            # Если IP не найден, получаем его с помощью ifconfig.me
+            response = requests.get("https://ifconfig.me")
+            public_ip = response.text.strip()
+            # Записываем новый IP в .env
+            with open(env_path, "a") as f:
+                f.write(f"SERVER_IP={public_ip}\n")
+else:
+    # Если файл .env не существует, создаем его и записываем IP
+    response = requests.get("https://ifconfig.me")
+    public_ip = response.text.strip()
+    with open(env_path, "w") as f:
+        f.write(f"SERVER_IP={public_ip}\n")
 
 print(f"Public IP: {public_ip}")
 
